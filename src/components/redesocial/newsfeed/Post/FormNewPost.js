e7b5282bee85eda19578339f0579128bb4dcc01f
Alexandre Torqueti <alexandre@globaltecnologia.net>
2024-03-10 23:23:53 -0300

    Criação de objetos do post

---
+++ src/components/redesocial/newsfeed/Post/FormNewPost.js
@@ -0,0 +1,135 @@
+import React  from 'react'
+import { Dropdown, Modal} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import CustomToggle from '../../../../components/dropdowns'
+
+//image
+import user1 from '../../../../assets/images/user/1.jpg'
+import img1 from '../../../../assets/images/small/07.png'
+import img2 from '../../../../assets/images/small/08.png'
+import img3 from '../../../../assets/images/small/09.png'
+import img4 from '../../../../assets/images/small/10.png'
+import img5 from '../../../../assets/images/small/11.png'
+import img6 from '../../../../assets/images/small/12.png'
+import img7 from '../../../../assets/images/small/13.png'
+import img8 from '../../../../assets/images/small/14.png'
+
+
+
+function FormNewPostComponent({show, handleClose}) {
+    
+    return (
+        <Modal size="lg" className="fade" id="post-modal" onHide={handleClose} show={show} >
+            <Modal.Header  className="d-flex justify-content-between">
+                <Modal.Title id="post-modalLabel">Create Post</Modal.Title>
+                <Link to="#" className="lh-1" onClick={handleClose} >
+                    <span className="material-symbols-outlined">close</span>
+                </Link>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="d-flex align-items-center">
+                    <div className="user-img">
+                        <img src={user1} alt="user1" className="avatar-60 rounded-circle img-fluid"/>
+                    </div>
+                    <form className="post-text ms-3 w-100 "  data-bs-toggle="modal" data-bs-target="#post-modal">
+                    <input type="text" className="form-control rounded" placeholder="Write something here..." style={{border:"none"}}/>
+                </form>
+                </div>
+                <hr/>
+                <ul className="d-flex flex-wrap align-items-center list-inline m-0 p-0">
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img1} alt="icon" className="img-fluid"/> Photo/Video</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img2} alt="icon" className="img-fluid"/> Tag Friend</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img3} alt="icon" className="img-fluid"/> Feeling/Activity</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img4} alt="icon" className="img-fluid"/> Check in</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img5} alt="icon" className="img-fluid"/> Live Video</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img6} alt="icon" className="img-fluid"/> Gif</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img7} alt="icon" className="img-fluid"/> Watch Party</div>
+                    </li>
+                    <li className="col-md-6 mb-3">
+                        <div className="bg-soft-primary rounded p-2 pointer me-3"><Link to="#"></Link>
+                        <img src={img8} alt="icon" className="img-fluid"/> Play with Friends</div>
+                    </li>
+                </ul>
+                <hr/>
+                <div className="other-option">
+                    <div className="d-flex align-items-center justify-content-between">
+                        <div className="d-flex align-items-center">
+                        <div className="user-img me-3">
+                            <img src={user1} alt="user1" className="avatar-60 rounded-circle img-fluid"/>
+                        </div>
+                        <h6>Your Story</h6>
+                        </div>
+                        <div className="card-post-toolbar">
+                            <Dropdown>
+                                <Dropdown.Toggle as={CustomToggle} role="button">
+                                <span className="btn btn-primary">Friend</span>
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu className=" m-0 p-0">
+                                    <Dropdown.Item className=" p-3" to="#">
+                                        <div className="d-flex align-items-top">
+                                        <i className="ri-save-line h4"></i>
+                                        <div className="data ms-2">
+                                            <h6>Public</h6>
+                                            <p className="mb-0">Anyone on or off Facebook</p>
+                                        </div>
+                                        </div>
+                                    </Dropdown.Item>
+                                    <Dropdown.Item className="p-3" to="#">
+                                        <div className="d-flex align-items-top">
+                                        <i className="ri-close-circle-line h4"></i>
+                                        <div className="data ms-2">
+                                            <h6>Friends</h6>
+                                            <p className="mb-0">Your Friend on facebook</p>
+                                        </div>
+                                        </div>
+                                    </Dropdown.Item>        
+                                    <Dropdown.Item className=" p-3" to="#">
+                                        <div className="d-flex align-items-top">
+                                        <i className="ri-user-unfollow-line h4"></i>
+                                        <div className="data ms-2">
+                                            <h6>Friends except</h6>
+                                            <p className="mb-0">Don't show to some friends</p>
+                                        </div>
+                                        </div>
+                                    </Dropdown.Item>
+                                    <Dropdown.Item className=" p-3" to="#">
+                                        <div className="d-flex align-items-top">
+                                        <i className="ri-notification-line h4"></i>
+                                        <div className="data ms-2">
+                                            <h6>Only Me</h6>
+                                            <p className="mb-0">Only me</p>
+                                        </div>
+                                        </div>
+                                    </Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            </div>
+                        </div>
+                    </div>
+                <button type="submit" className="btn btn-primary d-block w-100 mt-3">Post</button>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default FormNewPostComponent
--- src/store/setting/selectors.ts
+++ src/store/setting/selectors.ts
@@ -1,20 +1,47 @@
+interface Setting {
+    saveLocal: string;
+    setting: {
+      app_name: { value: string };
+      theme_scheme: { value: string };
+      theme_scheme_direction: { value: string };
+      theme_style_appearance: { value: string[] };
+      theme_color: string;
+      theme_transition: { value: string };
+      theme_font_size: { value: string };
+      page_layout: { value: string };
+      header_navbar: { value: string };
+      header_banner: { value: string };
+      sidebar_color: { value: string };
+      sidebar_type: { value: string[] };
+      sidebar_menu_style: { value: string };
+      footer: { value: string };
+      body_font_family: { value: string };
+      heading_font_family: { value: string };
+    };
+  }
+  
+  interface State {
+    setting: Setting;
+  }
+  
 // Selectors
-export const saveLocal = (state): String => state.setting.saveLocal;
-export const app_name = (state): String => state.setting.setting.app_name.value;
-export const theme_scheme = (state): String => state.setting.setting.theme_scheme.value;
-export const theme_scheme_direction = (state): String => state.setting.setting.theme_scheme_direction.value;
-export const theme_style_appearance = (state): String[] => state.setting.setting.theme_style_appearance.value;
-export const theme_color = (state): String => state.setting.setting.theme_color;
-export const theme_transition = (state): String => state.setting.setting.theme_transition.value;
-export const theme_font_size = (state): String => state.setting.setting.theme_font_size.value;
-export const page_layout = (state): String => state.setting.setting.page_layout.value;
-export const header_navbar = (state): String => state.setting.setting.header_navbar.value;
-export const header_banner = (state): String => state.setting.setting.header_banner.value;
-export const sidebar_color = (state): String => state.setting.setting.sidebar_color.value;
-export const sidebar_type = (state): String[] => state.setting.setting.sidebar_type.value;
-export const sidebar_menu_style = (state): String  => state.setting.setting.sidebar_menu_style.value;
-export const footer = (state): String => state.setting.setting.footer.value;
-export const body_font_family = (state): String => state.setting.setting.body_font_family.value;
-export const heading_font_family = (state): String => state.setting.setting.heading_font_family.value;
+export const saveLocal = (state: State): String => state.setting.saveLocal;
+export const app_name = (state: State): String => state.setting.setting.app_name.value;
+export const theme_scheme = (state: State): String => state.setting.setting.theme_scheme.value;
+export const theme_scheme_direction = (state: State): String => state.setting.setting.theme_scheme_direction.value;
+export const theme_style_appearance = (state: State): String[] => state.setting.setting.theme_style_appearance.value;
+export const theme_color = (state: State): String => state.setting.setting.theme_color;
+export const theme_transition = (state: State): String => state.setting.setting.theme_transition.value;
+export const theme_font_size = (state: State): String => state.setting.setting.theme_font_size.value;
+export const page_layout = (state: State): String => state.setting.setting.page_layout.value;
+export const header_navbar = (state: State): String => state.setting.setting.header_navbar.value;
+export const header_banner = (state: State): String => state.setting.setting.header_banner.value;
+export const sidebar_color = (state: State): String => state.setting.setting.sidebar_color.value;
+export const sidebar_type = (state: State): String[] => state.setting.setting.sidebar_type.value;
+export const sidebar_menu_style = (state: State): String  => state.setting.setting.sidebar_menu_style.value;
+export const footer = (state: State): String => state.setting.setting.footer.value;
+export const body_font_family = (state: State): String => state.setting.setting.body_font_family.value;
+export const heading_font_family = (state: State): String => state.setting.setting.heading_font_family.value;
 
-export const settingObj = (state): String => state.setting;
+export const settingObj = (state: State): Setting => state.setting;
+
