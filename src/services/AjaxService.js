187a8c331c5dc302d99f686595ae8527fb75ac3e
Alexandre Torqueti <alexandre@globaltecnologia.net>
2024-03-21 18:18:55 -0300

    Organizando e centralizando as chamadas ajax

---
+++ src/services/AjaxService.js
@@ -0,0 +1,50 @@
+import { ErrorService } from './ErrorService.js';
+
+export class AjaxService {
+
+    constructor(app = "") {
+        this.enderecoBase = 'http://localhost:8000/';
+        this.errorService = new ErrorService();
+        this.app = app;
+    }
+
+    /*
+    * Método para fazer um pedido GET
+    */
+    async get(endPoint) {
+        try {
+            const response = await fetch(this.enderecoBase + this.app + endPoint, {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            return response.json();
+        } catch(error) {
+            this.errorService.handlerError(error);
+            return null;
+        }
+    }
+    async postForm(endPoint, formData) {
+        try {
+            const response = await fetch(this.enderecoBase + this.app + endPoint, {
+                method: 'POST',
+                credentials: 'include',
+                body: formData 
+            });
+
+            if (!response.ok) {
+                throw new Error('Algo deu errado');
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            this.errorService.handlerError(error);
+            return null;
+        }
+    }
+    
+}
+
--- src/services/RedeSocial.js
+++ src/services/RedeSocial.js
@@ -1,103 +1,50 @@
+import { AjaxService } from './AjaxService.js';
+
+
 export class PostPaginado
 { 
+    constructor() {
+        this.AjaxService = new AjaxService('redesocial/');
+    }
     async get(paginaAtual) {
-        try {
-            const response = await fetch('http://localhost:8000/redesocial/api/posts/' + paginaAtual + '/***todos***', {
-                method: 'GET',
-                credentials: 'include',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-            if (!response.ok) {
-                throw new Error('Algo deu errado');
-            }
-            const data = await response.json();
-            return {
-                'continuar': data.continuar,
-                'pagina': data.posts
-            }
-        } catch (error) {
-            console.log(error);
+        const data = await this.AjaxService.get('api/posts/' + paginaAtual + '/***todos***');
+        return {
+            'continuar': data.continuar,
+            'pagina': data.posts
         }
     }
 
     // Método para salvar um post com os campos texto e imagem
     async post(text, fileIds) {
-        try {
-            // Criar um objeto FormData
-            const formData = new FormData();
-
-            // Adicionar o campo de texto ao formData
-            formData.append('content', text); // Supondo que 'post' tenha uma propriedade 'texto'
-
-            // Adicionar a imagem ao formData se existir
-            // Supondo que 'image' seja o arquivo de imagem enviado pelo usuário
-            if (fileIds.length > 0) {
-                formData.append('fileIds', fileIds);
-            }
-
-            const response = await fetch('http://localhost:8000/redesocial/api/posts', {
-                method: 'POST',
-                credentials: 'include',
-                // Remover 'Content-Type': 'application/json', o navegador irá definir o Content-Type corretamente para 'multipart/form-data'
-                body: formData // Usar formData como corpo do pedido
-            });
-
-            if (!response.ok) {
-                throw new Error('Algo deu errado');
-            }
-
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.log(error);
-            return null; // Retornar null ou manipular o erro conforme necessário
+        const formData = new FormData();
+        formData.append('content', text); // Supondo que 'post' tenha uma propriedade 'texto'
+        if (fileIds.length > 0) {
+            formData.append('fileIds', fileIds);
         }
+        const response = await this.AjaxService.postForm('api/posts', formData);
+
+        const data = await response.json();
+        return data;
     }
 
 }
 
 export class Icones
 {
+    constructor() {
+        this.AjaxService = new AjaxService('redesocial/');
+    }
     async get() {
-        try {
-            const response = await fetch('http://localhost:8000/redesocial/api/icones', {
-                method: 'GET',
-                credentials: 'include',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-            if (!response.ok) {
-                throw new Error('Algo deu errado');
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
+        return await this.AjaxService.get('api/icones');
     }
 }
 
 export class Fotos
 {
+    constructor() {
+        this.AjaxService = new AjaxService('redesocial/');
+    }
     async post() {
-        try {
-            const response = await fetch('http://localhost:8000/redesocial/api/fotos', {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-            if (!response.ok) {
-                throw new Error('Algo deu errado');
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
+        return await this.AjaxService.postJson('api/fotos');
     }    
 }
