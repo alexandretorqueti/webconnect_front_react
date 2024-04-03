4416fb6ffa153493a2a8c27011a49ab2017d7aa3
Alexandre Torqueti <alexandre@globaltecnologia.net>
2024-04-02 22:55:29 -0300

    Inicio do envio da mensagem

--- src/views/dashboard/app/chat/formSendMessage.js
+++ src/views/dashboard/app/chat/formSendMessage.js
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import {Link} from 'react-router-dom'
 import {Form, Button} from 'react-bootstrap'
-import { Socket } from '../../../../services/socket';
-// import ReactQuill from 'react-quill';
-// import 'react-quill/dist/quill.snow.css'; // importe o CSS do Quill
+import { useGlobalContext } from '../../../../GlobalContext';
 import './chat.css'
 
 
 const FormSendMessage = () => {
     const [message, setMessage] = useState('');
-
+    const { socket } = useGlobalContext();
     const handleSubmit = (e) => {
-        const SockerService = new Socket();
+        
 
         e.preventDefault();
 
@@ -19,7 +17,7 @@ const FormSendMessage = () => {
             return;
         }
 
-        SockerService.tenta_enviar(
+        socket.tenta_enviar(
             {
                 message: message,
                 pessoa_id_from: 1,
@@ -35,24 +33,12 @@ const FormSendMessage = () => {
 
 
     return (
-        <div className="chat-footer p-3 bg-white" style={{ 'min-height' : '100vh' }}>
+        <div className="chat-footer p-3 bg-white" style={{ 'minHeight' : '100vh' }}>
             <Form className="d-flex align-items-center" action="#" onSubmit={handleSubmit}>
                 <div className="chat-attagement d-flex">
                     <Link to="#"><i className="far fa-smile pe-3" aria-hidden="true"></i></Link>
                     <Link to="#"><i className="fa fa-paperclip pe-3" aria-hidden="true"></i></Link>
                 </div>
-                {/*<ReactQuill 
-                    theme="snow" 
-                    value={message} 
-                    onChange={setMessage} 
-                    className="me-3 react-quill-custom" 
-                    modules={{
-                        toolbar: [
-                            ['bold', 'italic', 'underline', 'strike'],
-                            ['image', 'code-block'],
-                        ]
-                    }}/>
-                */}
                 <Form.Control 
                     type="text" 
                     placeholder="Type your message" 
